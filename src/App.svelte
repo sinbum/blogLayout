b78b31b6d0b3d93748ac30977f166d24b44a7d17
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-10 22:22:21 +0900

    레이아웃 설정

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+    import {Router, Route} from "svelte-navigator";
+
+    export let name;
+    import Header from "~/components/header/Header.svelte";
+    import Nav from "@nav/Nav.svelte";
+    import Aside from "@side/sidebar.svelte"
+    // import Login from "@header/login/login.svelte"
+</script>
+
+
+<Router>
+    <nav>
+        <Nav/>
+    </nav>
+    <header>
+        <Header/>
+    </header>
+
+
+    <aside>
+        <Aside/>
+    </aside>
+
+
+    <div class="container">
+        <main>
+            <Route path="/">
+            </Route>
+        </main>
+    </div>
+
+</Router>
+
+
+<style>
+
+</style>
